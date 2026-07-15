6ae35bec7b06a09fcf15715c7be17e88493c7e0f
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-15 17:55:23 +0500

    Update: 3658.cpp

--- LeetCode/1.Easy/3658.cpp
+++ LeetCode/1.Easy/3658.cpp
@@ -13,6 +13,16 @@ public:
         return n;
     }
 };
+
+/*
+Explanation: 
+Sum of First n odd  = n²
+Sum of First n even = n(n+1)
+gcd(n²,n(n+1)) = n(gcd(n,n+1))
+n(gcd(n,n+1))  = n(1)  "Since gcd of consecutive ints = 1"
+n(1) = n
+*/
+
 int main() {
     Solution obj;
     int n=5;
