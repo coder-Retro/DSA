91b43d086acc615033b865de20fd6bdc38fff8fb
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-15 17:49:19 +0500

    Add: OddEven Sum GCD

---
+++ LeetCode/1.Easy/3658.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+/*
+Approach: Coprime Consecutive Integers
+TC: O(1)
+SC: O(1)
+*/
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+int main() {
+    Solution obj;
+    int n=5;
+    cout<<obj.gcdOfOddEvenSums(n);
+    return n;
+}
