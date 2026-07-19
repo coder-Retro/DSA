01b1ad8690cbc54572997a8dbe6695134075db60
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-19 16:23:42 +0500

    Fix: Kth Char in String

--- LeetCode/1.Easy/3304.cpp
+++ LeetCode/1.Easy/3304.cpp
@@ -3,23 +3,18 @@
 using namespace std;
 
 /*
-Approach: 
-TC: O()
-SC: O()
+Approach: Brian Kernighan's Algo / Bit Manipulation
+TC: O(log k), k = number of set bits in kth character
+SC: O(1)
 */
 
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s="a";
-        while(s.size()<k) {
-            string temp=s;
-            for(int i=0;i<temp.size();i++)
-                if(temp[i]<'z') temp[i]++;
-                else            temp[i]='a';
-            s+=temp;
-        }
-        return s[k-1];
+        int bits=0;
+        k--;
+        while(k) { k&=(k-1); bits++; }
+        return 'a'+bits;
     }
 };
 int main() {
