beb88d3e1b7526321c7df856d757d4e49350c268
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-27 12:47:01 +0500

    Add: Add Two Integers Bitwise

---
+++ LeetCode/2.Medium/371.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+/*
+Approach: Bit Manipulation / Carry Tracking
+TC: O(1)
+SC: O(1)
+*/
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b) {
+            int c=a&b;
+            a^=b;
+            b=c<<1;
+        }
+        return a;
+    }
+};
+int main() {
+    Solution obj;
+    int a=2;
+    int b=3;
+    cout<<obj.getSum(a,b);
+    return 0;
+}
