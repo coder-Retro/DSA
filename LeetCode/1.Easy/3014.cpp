d9603b7aab712e7ed976a20193e903f8b5ba08f7
coder-Retro <hasnainqadri9c@gmail.com>
2026-07-30 13:19:33 +0500

    Add: Max Pushes To Type Word I

---
+++ LeetCode/1.Easy/3014.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+Approach: Modulo Arithematics
+TC: O(1)
+SC: O(1)
+*/
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int blocks=word.size()/8;
+        int remain=word.size()%8;
+        int totalPush=0;
+        switch(blocks) {
+            case 0: totalPush+=(remain);      break;
+            case 1: totalPush+=(8+2*remain);  break;
+            case 2: totalPush+=(24+3*remain); break;
+            case 3: totalPush+=(48+4*remain); break;
+        }
+        return totalPush;
+    }
+};
+int main() {
+    Solution obj;
+    string s="xycdefghij";
+    cout<<obj.minimumPushes(s);
+    return 0;
+}
