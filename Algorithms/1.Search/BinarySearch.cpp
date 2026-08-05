a916d43ca71d4c2b74ff5287757637b2b0905150
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-06 00:26:30 +0500

    Fix: Formatting

--- Algorithms/1.Search/BinarySearch.cpp
+++ Algorithms/1.Search/BinarySearch.cpp
@@ -30,9 +30,9 @@ public:
         int mid;
         while(i<=j) {
             mid=i+(j-i)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[mid]<target) i=mid+1;
-            if(nums[mid]>target) j=mid-1;
+            if(nums[mid]==target)     return mid;
+            else if(nums[mid]<target) i=mid+1;
+            else                      j=mid-1;
         }
         return -1;
     }
