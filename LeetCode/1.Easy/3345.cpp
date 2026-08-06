cab02c34271a71c27fab287a5c6c3fb510f22ad1
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-06 12:01:22 +0500

    Add: Explanation

--- LeetCode/1.Easy/3345.cpp
+++ LeetCode/1.Easy/3345.cpp
@@ -23,6 +23,25 @@ public:
         return n;
     }
 };
+
+/*
+Explanation:
+The code itself is quite self explanatory but this explanation
+is aimed to show how the TC is O(1) despite the two loops that
+are running in this algo. We know that as soon as "n" becomes
+a multiple of 10, its digits' product will always be "0" hence
+it will always be divisible by "t", so this means that in the
+worst case, outer loop will run 9 times where 9 is a constant.
+Therefore outer loop is O(1). Now the inner loop, this runs as
+many times as the number of digits in current "n". But here is
+the catch, leetcode constraints say: 1 <= n <= 100. This shows
+that "n" can have maximum of 3 digits as it is capped at 100.
+So in the worst case, the inner loop will run 3 times, where 3
+is again a constant. Therefore inner loop is also O(1). Hence
+outer loop "worst case 9 iterations: O(1)" and the inner loop
+"worst case 3 iterations: O(1)" will together make a TC: O(1).
+*/
+
 int main() {
     Solution obj;
     int n=15;
