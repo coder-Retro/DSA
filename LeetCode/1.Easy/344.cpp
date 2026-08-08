3b29214e6232db5f11e4ffb752cc7b74e048eeb0
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-08 16:13:38 +0500

    Fix: Formatting

--- LeetCode/1.Easy/344.cpp
+++ LeetCode/1.Easy/344.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<utility>
 using namespace std;
 
 /*
@@ -11,8 +12,8 @@ SC: O(1)
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        for(int i=0,j=s.size()-1;i<j;i++,j--)
-            swap(s[i],s[j]);
+        int i=0,j=s.size()-1;
+        while(i<j) swap(s[i++],s[j--]);
     }
 };
 int main() {
