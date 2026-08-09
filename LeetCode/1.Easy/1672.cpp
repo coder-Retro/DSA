f58f7a649f60162a54a2466970f76c426532eda1
coder-Retro <hasnainqadri9c@gmail.com>
2026-08-09 17:28:58 +0500

    Add: Richest Customer

---
+++ LeetCode/1.Easy/1672.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+/*
+Approach: Max Row Sum
+TC: O(m*n), m and n are number of customers and their banks
+SC: O(1)
+*/
+
+class Solution {
+public:
+    int maxWealth(vector<vector<int>>& accounts) {
+        int maxW = 0;
+        for(vector<int>& account:accounts) {
+            int currW = 0;
+            for(int money:account) currW += money;
+            maxW = max(maxW, currW);
+        }
+        return maxW;
+    }
+};
+int main() {
+    Solution obj;
+    vector<vector<int>> accounts = {{2,8,7},{7,1,3},{1,9,5}};
+    cout << obj.maxWealth(accounts);
+    return 0;
+}
--- LeetCode/README.md
+++ LeetCode/README.md
@@ -26,7 +26,7 @@ By working through this section, you should be able to:
 
 ```text
 LeetCode/
-├── 1.Easy/       (107 problems)
+├── 1.Easy/       (108 problems)
 ├── 2.Medium/     (59 problems)
 ├── 3.Hard/       (5 problems)
 └── README.md
@@ -53,7 +53,7 @@ public:
 
 | Difficulty | Problems | Approach + TC/SC              |
 | :--------: | :------: | :---------------------------: |
-| 🟢 Easy    |    107   | ✅ Complete                   |
+| 🟢 Easy    |    108   | ✅ Complete                   |
 | 🟡 Medium  |    59    | ✅ Complete                   |
 | 🔴 Hard    |    5     | ✅ Complete                   |
 
