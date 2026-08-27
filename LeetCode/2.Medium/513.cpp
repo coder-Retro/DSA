9786f8b1f4093eb8f5404135bf52379ed76363cb
Kaido <hasnainqadri9c@gmail.com>
2026-08-27 12:02:00 +0500

    Add: Bottom Left Node

---
+++ LeetCode/2.Medium/513.cpp
@@ -0,0 +1,42 @@
+#include<vector>
+#include<queue>
+using namespace std;
+
+// Helper definition & Functions
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+/*
+Approach: Recursion / BFS
+TC: O(n)
+SC: O(n)
+*/
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        vector<vector<int>> levels;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            vector<int> level;
+            int s=q.size();
+            for(int i=0;i<s;i++) {
+                TreeNode* node=q.front();
+                q.pop();
+                level.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            levels.push_back(level);
+        }
+        return levels[levels.size()-1][0];
+    }
+};
