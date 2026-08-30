b19df87b0091b61f90f2d9c033c39b81b805382e
Kaido <hasnainqadri9c@gmail.com>
2026-08-30 17:49:00 +0500

    Update: Documentation

--- LeetCode/2.Medium/78.cpp
+++ LeetCode/2.Medium/78.cpp
@@ -11,13 +11,16 @@ SC: O(n 2ⁿ)
 
 class Solution {
     void helper(const vector<int>& v,vector<int>& curr,vector<vector<int>>& subs,int level) {
+        // Base Case Reached, Push Subset
         if(level==v.size()) {
             subs.push_back(curr);
             return;
         }
+        // Include Current Value
         curr.push_back(v[level]);
         helper(v,curr,subs,level+1);
-        curr.pop_back();
+        // Excluded Current Value
+        curr.pop_back(); // Backtracking
         helper(v,curr,subs,level+1);
     }
 public:
