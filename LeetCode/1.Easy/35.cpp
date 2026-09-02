eaab6001ab37a0f415169f0d556724b52b276392
Kaido <hasnainqadri9c@gmail.com>
2026-09-02 20:02:29 +0500

    Update: Test Case

--- LeetCode/1.Easy/35.cpp
+++ LeetCode/1.Easy/35.cpp
@@ -11,8 +11,7 @@ SC: O(1)
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1; 
+        int low=0,high=nums.size()-1; 
         while (low<=high) {
             int mid=low+(high-low)/2;
             if (nums[mid]==target)     return mid; // Target found
