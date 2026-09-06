df412a58c459ebb3405916e3656172e5ab247b52
Kaido <hasnainqadri9c@gmail.com>
2026-09-06 15:15:12 +0500

    Add: Remove Duplicates From Sorted Array

--- LeetCode/1.Easy/26.cpp
+++ LeetCode/1.Easy/26.cpp
@@ -11,24 +11,21 @@ SC: O(1)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        for(int i=0;i<nums.size()-1;i++) {
-            for(int j=i+1;j<nums.size();j++) {
-                if(nums[i]==nums[j]) {
-                    for(int k=j;k<nums.size()-1;k++)
-                        nums[k]=nums[k+1];
-                    j--;
-                    nums.pop_back();
-                }
-            }
+        int i=1,j=1;
+        while(j<nums.size()) {
+            if(nums[j]!=nums[i-1]) nums[i++]=nums[j];
+            j++;
         }
-        return nums.size();
+        return i;
     }
 };
 
 int main() {
     Solution obj;
     vector<int> nums={0,0,1,1,1,2,2,3,3,4};
-    obj.removeDuplicates(nums);
-    for(int i:nums) cout<<i<<" ";
+    int newSize=obj.removeDuplicates(nums);
+    for(int i=0;i<newSize;i++) {
+        cout<<nums[i]<<" ";
+    }
     return 0;
 }
