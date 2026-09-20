6ad1edce9e20619ad941a80b33567bb2266be206
Kaido <hasnainqadri9c@gmail.com>
2026-09-20 16:54:38 +0500

    Add: Reverse Degeree String

---
+++ LeetCode/1.Easy/3498.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+Approach: Simulation
+TC: O(n)
+SC: O(1)
+*/
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum=0;
+        for (int i=0;i<s.length();i++)
+            sum+=(i+1)*('z'+1-s[i]);
+        return sum;
+    }
+};
+
+int main() {
+    Solution obj;
+    string s="abc";
+    cout<<obj.reverseDegree(s);
+    return 0;
+}
